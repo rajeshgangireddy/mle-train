0aed87b13609d702aeb0d4dcc0e9e2d805898348
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2024-10-23 14:59:26 +0200

    fix a test case

--- tests/test_feature_engineering.py
+++ tests/test_feature_engineering.py
@@ -1,25 +1,31 @@
-import pandas as pd
+import numpy as np
+
 from src.data_handler.feature_engineering import FeatureEngineer
+
 
 class TestFeatureEngineering:
     def test_read_and_prepare_data(self):
         config = {
-            'feature_engineering': {
-                'datetime_features': ['date'],
-                'categorical_features': ['category'],
-                'target': 'target'
+            "feature_engineering": {
+                "datetime_features": ["date"],
+                "categorical_features": [
+                    "Temperature",
+                    "Humidity",
+                    "Light",
+                    "CO2",
+                    "HumidityRatio",
+                ],
+                "target": "Occupancy",
             },
-            'data': {
-                'path': 'tests/data/test_data.csv',
-                'ignore_columns': [],
-                'index_column': None
-            }
+            "data": {
+                "path": "tests/data/datatest.txt",
+                "ignore_columns": [],
+                "index_column": 0,
+            },
         }
         feature_engineer = FeatureEngineer(config)
-        X, y = feature_engineer.read_and_prepare_data()
-        assert isinstance(X, pd.DataFrame)
-        assert isinstance(y, pd.Series)
-        assert X.shape[1] == 2
-        assert y.shape[0] == 3
-        assert X.columns.tolist() == ['feature1', 'feature2']
-        assert y.tolist() == [1, 2, 3]
+        x, y = feature_engineer.read_and_prepare_data()
+        assert isinstance(x, np.ndarray)
+        assert isinstance(y, np.ndarray)
+        assert x.shape[1] == 5  # 5 features (and should have excluded the date column)
+        assert y.shape[0] == 2665  # 2665 data points
