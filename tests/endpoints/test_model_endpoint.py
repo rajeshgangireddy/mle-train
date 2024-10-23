16f643ea5d4aee4ca767f1a2f55db8bce087b658
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2024-10-23 15:47:46 +0200

    Move test files into respective directories

--- tests/endpoints/test_model_endpoint.py
+++ tests/endpoints/test_model_endpoint.py
@@ -1,6 +1,8 @@
-import unittest
 import json
+import unittest
+
 import requests
+
 
 class TestModelEndpoint(unittest.TestCase):
     def setUp(self):
@@ -20,17 +22,20 @@ class TestModelEndpoint(unittest.TestCase):
                 "Light": 450,
                 "CO2": 420,
                 "HumidityRatio": 0.005,
-            }
+            },
         ]
         self.sample_output = [0, 0]
 
     def test_predict(self):
-        response = requests.post(self.url, headers=self.headers, data=json.dumps(self.sample_data))
+        response = requests.post(
+            self.url, headers=self.headers, data=json.dumps(self.sample_data)
+        )
         self.assertEqual(response.status_code, 200)
         predictions = response.json()
         self.assertIsInstance(predictions, list)
         self.assertEqual(len(predictions), len(self.sample_data))
         self.assertEqual(predictions, self.sample_output)
+
 
 if __name__ == "__main__":
     unittest.main()
